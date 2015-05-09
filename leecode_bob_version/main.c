750b57186d36aada97e46fee5cc001e8b79921e8
soledad <wangboshine@126.com>
2015-05-09 17:01:45 +0800

    change some files

--- leecode_bob_version/main.c
+++ leecode_bob_version/main.c
@@ -10,19 +10,24 @@ int main(int arc, char *argv[])
 
     int num = 10;
     int A[MAXMUM] = {1,2,3,4,4,4,4,7,7,8};
+
+
     for (i = 0; i < num; i++)
         printf("%d  ", A[i]);
     printf("\n");
 
     int num_new;
     //num_new = remove_duplicates(A, num);
-    num_new = remove_duplicates3(A, num);
-
-
+    //num_new = remove_duplicates3(A, num);
+    int B[MAXMUM] = {6,7,8,9, 0,1,2,3,4,5};
+    int x = 6;
+    num_new = search_in_rotated_sorted_array(x, B, num);
+    printf("num_new: %d\n", num_new);
+    /*
     for (i = 0; i < num_new; i++)
         printf("%d  ",A[i]);
     printf("\n");
-
+    */
     return 0;
 }
 
--- leecode_bob_version/search_in_rotated_sorted_array.c
+++ leecode_bob_version/search_in_rotated_sorted_array.c
@@ -2,10 +2,27 @@
 
 int search_in_rotated_sorted_array(int x, int A[], int num)
 {
-	int index; 
-	for (index = 0; index < num; index++)
-		if ( A[index] > A[index+1])
-			break;
-    return index;
+    int first = 0, last = num;
+    while (first != last)
+    {
+        const int mid = first + (last - first) / 2;
+        if (A[mid] == x)
+            return mid;
+        if (A[first] <= A[mid])
+        {
+            if (A[first] <= x && x < A[mid])
+                last = mid;
+            else
+                first = mid + 1;
+        }
+        else
+        {
+            if (A[mid] < x && x <= A[last - 1])
+                first = mid - 1;
+            else
+                last = mid;
+        }
+    }
+    return -1;
 
 }
